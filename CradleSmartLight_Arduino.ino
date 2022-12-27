5f17455d717fcf26327d7ce233a51f687a59cb43
Leonardo Cavagnis <leonardo.cavagnis@hotmail.it>
2022-12-27 23:04:22 +0100

    led brightness and led color management via BLE

--- CradleSmartLight_Arduino.ino
+++ CradleSmartLight_Arduino.ino
@@ -1,6 +1,8 @@
 #include <ArduinoBLE.h>
 #include <Adafruit_NeoPixel.h>
 
+//TODO: Value in EEPROM
+
 #define LED_PIN       6
 #define LED_NUM       30
 
@@ -8,8 +10,14 @@ Adafruit_NeoPixel ledstrip(LED_NUM, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 BLEService            cradlesmartlightService     ("c9ea4800-ad9e-4d67-b570-69352fdc1078");
 BLEByteCharacteristic ledstatusCharacteristic     ("c9ea4801-ad9e-4d67-b570-69352fdc1078", BLERead | BLEWrite);
-BLEByteCharacteristic ledcolorCharacteristic      ("c9ea4802-ad9e-4d67-b570-69352fdc1078", BLERead | BLEWrite);
+BLECharacteristic     ledcolorCharacteristic      ("c9ea4802-ad9e-4d67-b570-69352fdc1078", BLERead | BLEWrite, 3);
 BLEByteCharacteristic ledbrightnessCharacteristic ("c9ea4803-ad9e-4d67-b570-69352fdc1078", BLERead | BLEWrite);
+
+bool          led_status      = false;
+unsigned char led_brightness  = 0;
+unsigned char led_color_red   = 0;
+unsigned char led_color_green = 0;
+unsigned char led_color_blue  = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -40,7 +48,6 @@ void setup() {
 
   // set the initial value for the characeristic:
   ledstatusCharacteristic.writeValue(0);
-  ledcolorCharacteristic.writeValue(0);
   ledbrightnessCharacteristic.writeValue(0); 
   
   // start advertising
@@ -61,25 +68,53 @@ void loop() {
 
     // while the central is still connected to peripheral:
     while (central.connected()) {
-      // if the remote device wrote to the characteristic,
-      // use the value to control the LED:
+      // Check LedStatus characteristic write
       if (ledstatusCharacteristic.written()) {
-        if (ledstatusCharacteristic.value()) {   // any value other than 0
+        if (ledstatusCharacteristic.value()) {   
           Serial.println("LED on");
+          led_status = true;
           
           ledstrip.clear();
-          ledstrip.setBrightness(255);
+          ledstrip.setBrightness(led_brightness);
           for (int led_i=0; led_i<LED_NUM; led_i++) {
-            ledstrip.setPixelColor(led_i, ledstrip.Color(255, 255, 255));
+            ledstrip.setPixelColor(led_i, ledstrip.Color(led_color_red, led_color_green, led_color_blue));
           }
           ledstrip.show();
-        } else {                              // a 0 value
+        } else {                              
           Serial.println(F("LED off"));
+          led_status = false;
           
           ledstrip.clear();
           ledstrip.show();
         }
       }
+      
+      // Check LedColor characteristic write
+      if (ledcolorCharacteristic.written()) {
+        if (ledcolorCharacteristic.valueLength() == 3) {
+          unsigned char dataRawColor[3] = {};
+          ledcolorCharacteristic.readValue(dataRawColor, 42);
+
+          led_color_red   = dataRawColor[0];
+          led_color_green = dataRawColor[1];
+          led_color_blue  = dataRawColor[2];
+
+          for (int led_i=0; led_i<LED_NUM; led_i++) {
+            ledstrip.setPixelColor(led_i, ledstrip.Color(led_color_red, led_color_green, led_color_blue));
+          }
+
+          if (led_status) ledstrip.show();
+        }
+      }
+
+      // Check LedBrightness characteristic write
+      if (ledbrightnessCharacteristic.written()) {
+        led_brightness = ledbrightnessCharacteristic.value();
+        ledstrip.setBrightness(led_brightness);
+        
+        if (led_status) ledstrip.show();
+      }
+
     }
 
     // when the central disconnects, print it out:
